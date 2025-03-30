1e3c26a34d8854c0af34c7331c1d8aa2bfc5aa84
LENOVO <manasi.dwivedi09@gmail.com>
2025-03-30 23:06:09 +0530

    30 Mar 2025 pushing test case

---
+++ src/Mar/ex_02032025_Main/Lab001.java
@@ -0,0 +1,7 @@
+package Mar.ex_02032025_Main;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ src/Mar/ex_02032025_Main/Lab002_HelloWorldProgram.java
@@ -0,0 +1,7 @@
+package Mar.ex_02032025_Main;
+
+public class Lab002_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld");
+    }
+}
